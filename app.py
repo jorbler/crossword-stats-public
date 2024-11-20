1125c9e2096193bd632669923ce44b908b38f58c
jorbler <151876688+jorbler@users.noreply.github.com>
2024-11-20 14:57:01 -0500

    Continuing to build GUI

--- app.py
+++ app.py
@@ -1,106 +1,24 @@
 from src.app_components import *
+
 from PyQt5 import QtWidgets as qtw
+from PyQt5.QtGui import QFont
 import sys
 
 class SimpleApp(qtw.QApplication):
-    '''
-    Contains a VerticalHelloBox to
-    prompt the user for their name and say hello!
-    '''
-    def __init__(self, **kwargs):
-        super().__init__([])
-
-        # Add the VerticalHelloBox
-        self.main_widget = WelcomeWindow()
-
-        # Run the application
-        self.main_widget.show()
-        sys.exit(self.exec_())
-        return
-
-class WindowWithVerticalSlots(qtw.QWidget):
-    '''
-    A window with a title and an empty
-    vertical container (QVBoxLayout).
-    
-    Intended use is to inherit and add
-    additional customization
-    '''
-    def __init__(self, title: str):
-        super().__init__()
-        
-        # Make a title for the window
-        self.setWindowTitle(title)
-        
-        # Create an empty vertical layout container
-        self.my_layout = qtw.QVBoxLayout(self)
-        return
-
-class WelcomeWindow(WindowWithVerticalSlots):
-    '''
-    Contains a textbox and a button the user can
-    press which opens up another window for them to
-    enter their name
-    '''
     def __init__(self):
-        super().__init__(title = "Welcome!")
-        self.configure()
-        return
-    
-    def configure(self):
-        self.greeting_box = qtw.QLabel(self)
-        self.hello_button = qtw.QPushButton('Who are you?', self)
-        
-        # Bind the hello_button_clicked function to 
-        # the clicked event of the hello_button
-        self.hello_button.clicked.connect(self.hello_button_clicked)
-        
-        # Add the widgets to the layout
-        self.my_layout.addWidget(self.greeting_box)
-        self.my_layout.addWidget(self.hello_button)
-        return
-    
-    def hello_button_clicked(self):
-        # Create a new input popup
-        name_getter = InputPopup('Who are you?')
+        super().__init__([])
+        self.welcome_box()
 
-        # This runs the dialog box and waits for the user to click
-        # the ok button.
-        if name_getter.exec_() == qtw.QDialog.Accepted:
-            # Update the text in the main window with the text entered
-            # by the user
-            self.greeting_box.setText(f'Hello {name_getter.get_text()}!')
-        return
-    
-class InputPopup(qtw.QDialog):
-    '''
-    A popup window with a text box and an OK button
-    to allow a user to enter some freeform text.
-    '''
-    def __init__(self, title: str):
-        super().__init__()
-        self.setWindowTitle(title)
+    def welcome_box(self) -> None:
+        intro_text = "Welcome to MyCrosswordBuddy! This app allows you to gain insight into your NYT Crossword data. \nTo access your data, you will need to enter the cookie associated with your NYT Games account.\n\nDo you need instructions on how to access your cookie?"
+        welcome = YesNoPopUpWindow("Welcome!", intro_text)
+        if welcome.exec() == qtw.QMessageBox.Yes:
+            self.cookie_instructions()
+        else: 
+            print("no")
 
-        # Text box to enter your name
-        self.name_entry = qtw.QLineEdit(self)
+    def cookie_instructions(self) -> None:
+        IntroMenuPages()
 
-        # Button to click when you are done entering your name
-        self.ok_button = qtw.QPushButton("Ok", self)
-
-        # When the button is clicked, calls the accept()
-        # method of the QDialog, which lets the app know
-        # that the interaction with the dialog is complete
-        self.ok_button.clicked.connect(self.accept)
-        
-        # Create a vertical layout and add the widgets
-        self.my_layout = qtw.QVBoxLayout(self)
-        self.my_layout.addWidget(self.name_entry)
-        self.my_layout.addWidget(self.ok_button)
-        return
-    
-    def get_text(self) -> str:
-        # Get the name that was entered into the text box
-        return self.name_entry.text()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     SimpleApp()
--- src/app_components.py
+++ src/app_components.py
@@ -1,32 +1,84 @@
 from PyQt5 import QtWidgets as qtw
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont, QPixmap
 import sys
+import os
 
-class WelcomeDialog(qtw.QMessageBox):
+class YesNoPopUpWindow(qtw.QMessageBox):
+    '''
+    Creates a dialog box with body text
+    '''
+    def __init__(self, title, body_text):
+        super().__init__()
+        
+        self.setWindowTitle(title)
+        self.setText(body_text)
+        self.setStandardButtons(qtw.QMessageBox.Yes | qtw.QMessageBox.No)
+        self.setDefaultButton(qtw.QMessageBox.Yes)
+        self.setFont(QFont("Arial", 14, weight = 1))
+
+class WelcomeWindow(YesNoPopUpWindow):
+    '''
+    Welcome window that appears to the user the first time that they open the app.
+    '''
+    def __init__(self):
+        intro_text = "Welcome to MyCrosswordBuddy! This app allows you to gain insight into your NYT Crossword data. \nTo access your data, you will need to enter the cookie associated with your NYT Games account.\n\nDo you need instructions on how to access your cookie?"
+        super().__init__("Welcome!", intro_text)
+
+class MenuPage(qtw.QVBoxLayout):
+    def __init__(self, text, rel_image_path = None):
+        super().__init__()
+        self.create_page(text, rel_image_path)
+
+    def create_page(self, text, rel_image_path = None):
+        page_text = qtw.QLabel()
+        page_text.setText(text)
+        self.addWidget(page_text)
+        if rel_image_path:
+            page_image = qtw.QLabel()
+            page_image.setPixmap(QPixmap(os.getcwd() + rel_image_path))
+            self.addWidget(page_image)
+
+
+class IntroMenuPages(qtw.QComboBox):
+    '''
+    Menu that has next and previous arrows that switch the "page" of the menu.
+    '''
     def __init__(self):
         super().__init__()
+        self.create_pages()
+    
+    def create_pages(self):
+        stacked_widget =  qtw.QStackedWidget()
 
-        self.setWindowTitle("Welcome!")
+        page1 = MenuPage("Open https://www.nytimes.com/crosswords in Google Chrome.\n\nLog in to your account if not already logged in.")
+        page2 = MenuPage("Click on the three dots in the top right corner of the window.", "src/images/menu1.png")
+        page3 = MenuPage("Click on \"More Tools\" and then \"Developer Tools\"", "src/images/menu2.png")
+        page4 = MenuPage("Click on \"Application\"", "src/images/menu3.png")
+        page5 = MenuPage(" On the left panel, click \"Cookies\"; this will show a drop-down menu below. Click on \"https://nytimes.com\"", "src/images/menu4.png")
+        page6 = MenuPage(""" Under the \"Name\" column, scroll down to find \"NYT-S\". 
+                         The value for your cookie will be in the \"Value\" column, however it will be truncated. 
+                         Click on \"NYT-S\" in the \"Name\" column. Your full cookie will be in the bottom panel (showed below). 
+                         Highlight the full cookie and copy it.""", "src/images/menu5.png")
+        
+        stacked_widget.addWidget(page1)
+        stacked_widget.addWidget(page2)
+        stacked_widget.addWidget(page3)
+        stacked_widget.addWidget(page4)
+        stacked_widget.addWidget(page5)
+        stacked_widget.addWidget(page6)
 
-        self.needs_instructions = False
 
-        layout = qtw.QVBoxLayout()
+        self.addItem("Step 1")
+        self.addItem("Step 2")
+        self.addItem("Step 3")
+        self.addItem("Step 4")
+        self.addItem("Step 5")
+        self.addItem("Step 6")
+        
+        self.activated[int].connect(stacked_widget.setCurrentIndex)
 
-        intro_text = """
-                    Welcome to MyCrosswordBuddy! This app allows you to gain insight into your NYT Crossword data. 
-                    To access your data, you will need to enter the cookie associated with your NYT Games account.
-                    Do you need instructions on how to access your cookie?
-                    """
-        welcome_label = qtw.QLabel(intro_text, self)
-        layout.addWidget(welcome_label)
 
-        # yes_button = qtw.QPushButton("Yes")
-        # no_button = qtw.QPushButton("No")
+        
 
-        # yes_button.clicked.connect(self.set_instructions_needed)
-        # no_button.clicked.connect(self.close_dialog)
 
-        # layout.addWidget(yes_button)
-        # layout.addWidget(no_button)
-
-        self.setLayout(layout)
